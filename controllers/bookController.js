d38ef1224ccd66b042988afebf0891e59bf4a2a3
Kyle Czajkowski <skiroyjenkins@gmail.com>
2018-12-05 16:27:19 -0700

    added put route for books

--- controllers/bookController.js
+++ controllers/bookController.js
@@ -3,23 +3,23 @@ const knex = require('../db/connection')
 const getAll = (req, res, next) => {
     return knex('book')
         .orderBy('id', 'asc')
-        .then(books => res.json({books}))
+        .then(books => res.json({ books }))
         .catch(err => console.error("Error:", err))
 }
 
 const getOne = (req, res, next) => {
     const id = req.params.id
-    if(!Number(id)){
-        res.status(404).json({error: 'Please enter a valid id'})
+    if (!Number(id)) {
+        res.status(404).json({ error: 'Please enter a valid id' })
     } else {
         return knex('book')
             .select('*')
             .where('id', id)
             .then(book => {
-                if(!book.length){
-                    res.status(404).json({error: 'That book doesn\'t exist yet'})
+                if (!book.length) {
+                    res.status(404).json({ error: 'That book doesn\'t exist yet' })
                 } else {
-                    return res.json({book})
+                    return res.json({ book })
                 }
             })
             .catch(err => console.error("Error:", err))
@@ -27,36 +27,47 @@ const getOne = (req, res, next) => {
 }
 const postBook = (req, res, next) => {
     const body = req.body
-    if(!body.title || !body.genre || ! body.description || !body.coverURL){
-        res.status(400).json({error: "Please fill out all parts of the form to add a book."})
+    if (!body.title || !body.genre || !body.description || !body.coverURL) {
+        res.status(400).json({ error: "Please fill out all parts of the form to add a book." })
     } else {
         return knex('book')
             .insert(body)
             .returning('*')
-            .then(book => res.json({book: book[0]}))
+            .then(book => res.json({ book: book[0] }))
             .catch(err => console.error("Error:", err))
     }
 }
-// const editBook = (req, res, next) => {
-//     return 
-// }
-const deleteBook = (req, res, next) => {
+const editBook = (req, res, next) => {
     const id = req.params.id
-    if(!Number(id)){
-        res.status(404).json({error: 'Please enter a valid id'})
+    const body = req.body
+    if (!Number(id)) {
+        return res.status(404).json({ error: "Please enter a valid ID number" })
     } else {
         return knex('book')
             .where('id', id)
-            .then(book => {
-                if(!book.length){
-                    return res.status(404).json({error: 'Please enter a valid id'})
-                } else {
-                    return book
-                }
-            })
+            .update(body)
+            .returning('*')
+            .then(book => res.json({book: book[0]}))
+    }
+}
+
+const deleteBook = (req, res, next) => {
+    const id = req.params.id
+    if (!Number(id)) {
+        res.status(404).json({ error: 'Please enter a valid id' })
+    } else {
+        return knex('book')
+            .where('id', id)
+            // .then(book => {
+            //     if(!book.length){
+            //         return res.status(404).json({error: 'Please enter a valid id'})
+            //     } else {
+            //         return book
+            //     }
+            // })
             .delete()
             .returning('*')
-            .then(book =>res.json({deleted: book[0]}))
+            .then(book => res.json({ deleted: book[0] }))
     }
 }
 
@@ -64,6 +75,6 @@ module.exports = {
     getAll,
     getOne,
     postBook,
-    //editBook,
+    editBook,
     deleteBook
 }
--- routes/bookRoutes.js
+++ routes/bookRoutes.js
@@ -5,7 +5,7 @@ const controller = require('../controllers/bookController')
 router.get('/', controller.getAll)
 router.get('/:id', controller.getOne)
 router.post('/', controller.postBook)
-// router.put('/:id', controller.editBook)
-router.delete(':id', controller.deleteBook)
+router.put('/:id', controller.editBook)
+router.delete('/:id', controller.deleteBook)
 
 module.exports = router
